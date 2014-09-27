5c0e7922577f119c7fdbd25cc83a2c30a2e52f3e
ckelner <chris.kelner@gmail.com>
2014-09-27 12:30:04 -0400

    add some HIV :bomb:

--- game/states/play.js
+++ game/states/play.js
@@ -1,40 +1,110 @@
+// shitty global variables
 // static sized game window (sux)
 global_HEIGHT=940;
 global_WIDTH=1662;
 // White blood cell height
 global_WBC_HEIGHT=32;
+// HIV height
+global_HIV_HEIGHT=32;
 // array to keep WBC objects
 global_whiteBloodCellArr=[];
 // WBC counter
 global_whiteBloodCellNum=0;
+// array to keep HIV objects
+global_hivCellArr=[];
+// HIV counter
+global_hivCellNum=0;
+// shitty global functions
+function global_findCellObjByIndexValue ( arr, index ) {
+  var arrLen = arr.length;
+  for (var i = 0; i < arrLen; i++) {
+    if( arr[i].index === index ) {
+      return i;
+    }
+  }
+  return null;
+}
 
   'use strict';
   function Play() {}
   Play.prototype = {
     create: function() {
       // I don't know
-      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+      this.game.physics.startSystem( Phaser.Physics.ARCADE );
       // how often to spawn white blood cells
-      var x = this.game.time.events.loop(1000, this.createWhiteBloodCell, this);
+      var wbcEventLoop = this.game.time.events.loop( 1000, this.createWhiteBloodCell, this );
+      // how often to spawn HIV
+      var hivEventLoop = this.game.time.events.loop( 1000, this.createHIV, this );
     },
     // update loop
     update: function() {},
+    /*
+    Do this shit later
+    genericCreateThing: function( startHeight, startWidth, spriteName, endHeight,
+      tweenTime, thingIndex, thingArray ) {
+      var sprite = this.game.add.sprite(
+        this.randomNum( startWidth ),
+        startHeight,
+        spriteName
+      );
+      var tween = this.game.add.tween( sprite );
+      tween.to({ y: endHeight }, tweenTime);
+      var thing = {
+        "index": thingIndex,
+        "tween": tween, // tween index
+        "sprite": sprite // the sprite
+      };
+      thingArray.push( thing );
+    },*/
+    /*
+      Creates HIV
+    */
+    createHIV: function() {
+      var sprite = this.game.add.sprite(
+        this.randomNum( global_WIDTH ), // place the HIV at random starting x value
+        0 - global_HIV_HEIGHT, // place HIV at bottom of screen
+        "hiv_32" // "hiv" == the preloaded image from preload.js
+      );
+      // Create a tween on the HIV
+      var tween = this.game.add.tween( sprite );
+      /*
+        Tween will move south to north over 6 secs
+        TODO: Replace with pathfinding once we have enemies (HIV)?
+      */
+      tween.to({ y: global_HEIGHT + global_HIV_HEIGHT }, 6000); // to test, change y to "0" or less to watch it delete
+      // create the HIV object
+      var HIV = {
+        "index": global_hivCellNum, // index HIV -- age
+        "tween": tween, // tween index
+        "sprite": sprite // the sprite
+      };
+      // store the HIV object in array
+      global_hivCellArr.push( HIV );
+      // index of given object
+      var dex = global_hivCellNum + 0;
+      // define what happens when tween finishes
+      tween.onComplete.add( this.removeHIV, dex );
+      // start tweening
+      tween.start();
+      // ++ bitch
+      global_hivCellNum++;
+    },
     /*
       Creates white blood cells
     */
     createWhiteBloodCell: function() {
       var sprite = this.game.add.sprite(
-        this.randomNum(global_WIDTH), // place the WBC at random starting x value
+        this.randomNum( global_WIDTH ), // place the WBC at random starting x value
         global_HEIGHT, // place WBC at bottom of screen
         "wbc" // "wbc" == the preloaded image from preload.js
       );
       // Create a tween on the WBC
-      var tween = this.game.add.tween(sprite);
+      var tween = this.game.add.tween( sprite );
       /*
         Tween will move south to north over 6 secs
         TODO: Replace with pathfinding once we have enemies (HIV)?
       */
-      tween.to({ y: -global_WBC_HEIGHT }, 6000);
+      tween.to({ y: -global_WBC_HEIGHT }, 6000); // to test, change y to "0" or less to watch it delete
       // create the WBC object
       var WBC = {
         "index": global_whiteBloodCellNum, // index WBC -- age
@@ -42,9 +112,10 @@ global_whiteBloodCellNum=0;
         "sprite": sprite // the sprite
       };
       // store the WBC object in array
-      var index = global_whiteBloodCellArr.push(WBC) - 1;
+      global_whiteBloodCellArr.push( WBC );
+      var dex = global_whiteBloodCellNum + 0;
       // define what happens when tween finishes
-      tween.onComplete.add(this.removeWBC,index);
+      tween.onComplete.add( this.removeWBC, dex );
       // start tweening
       tween.start();
       // ++ bitch
@@ -52,16 +123,37 @@ global_whiteBloodCellNum=0;
     },
     // generates 1 to "num"
     randomNum: function( num ) {
-      return Math.floor(Math.random() * num) + 1;
+      return Math.floor( Math.random() * num ) + 1;
     },
     // removes WBC
     removeWBC: function() {
-      // get the WBC
-      var WBC = global_whiteBloodCellArr[this.valueOf()];
-      // delete sprite form game memory
-      WBC.sprite.destroy(true);
-      // remove 1 WBC at index in arrays
-      global_whiteBloodCellArr.splice(this.valueOf(),1);
+      // find WBC index to remove
+      var indexToRemove = global_findCellObjByIndexValue( global_whiteBloodCellArr, this.valueOf() );
+      if( indexToRemove === null ) {
+        console.log( "WBC index to remove was null... #wtf" );
+      } else {
+        // get the WBC
+        var WBC = global_whiteBloodCellArr[ indexToRemove ];
+        // remove 1 WBC at index in array
+        global_whiteBloodCellArr.splice( indexToRemove, 1);
+        // delete sprite form game memory
+        WBC.sprite.destroy( true );
+      }
+    },
+    // removes HIV
+    removeHIV: function() {
+      // find HIV index to remove
+      var indexToRemove = global_findCellObjByIndexValue( global_hivCellArr, this.valueOf() );
+      if( indexToRemove === null ) {
+        console.log( "HIV index to remove was null... #wtf" );
+      } else {
+        // get the HIV
+        var HIV = global_hivCellArr[ indexToRemove ];
+        // remove 1 HIV at index in array
+        global_hivCellArr.splice( indexToRemove, 1);
+        // delete sprite form game memory
+        HIV.sprite.destroy( true );
+      }
     }
   };
   
--- game/states/preload.js
+++ game/states/preload.js
@@ -9,13 +9,12 @@ Preload.prototype = {
   preload: function() {
     this.asset = this.add.sprite(this.HEIGHT/2,this.WIDTH/2, 'preloader');
     this.asset.anchor.setTo(0.5, 0.5);
-
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     this.load.setPreloadSprite(this.asset);
     this.load.image('hiv', 'assets/HIV-virus.png');
     this.load.image('heart', 'assets/heart.png');
     this.load.image('wbc', 'assets/white-blood-cell-32.png');
-
+    this.load.image('hiv_32', 'assets/hiv.png'); // 32x32
   },
   create: function() {
     this.asset.cropEnabled = false;
